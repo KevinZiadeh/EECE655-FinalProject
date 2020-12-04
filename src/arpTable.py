35108673be08305837f102325af60235fd937a41
Kevin Ziadeh <kevinziadeh@gmail.com>
2020-12-04 12:37:41 +0200

    Added additional help

--- src/arpTable.py
+++ src/arpTable.py
@@ -2,6 +2,12 @@
 Import OS to detect windows or Linux
     command = "ip neigh" arp table on linux
     command = "arp -a" arp table on Windows
+
+
+import requests
+
+def get_info(mac_address):
+	return requests.get("https://macvendors.co/api/vendorname/"+mac_address+"/").text
 '''
 
 # commandOutput = str(subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)).split('\\n')
@@ -11,4 +17,4 @@ Import OS to detect windows or Linux
 
 # commandOutput = out.split('\\r\\n')
 # routerIP = commandOutput[3].split()[0]
-# routerMAC = commandOutput[3].split()[1]
+# routerMAC = commandOutput[3].split(
