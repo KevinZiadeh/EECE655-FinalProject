20f87f20dcb843e7f4979ead71a3b674583ce269
hsandid <hadisandid@gmail.com>
2020-12-04 16:51:56 +0200

    Modified the arpTable.py and OUICheck.py

--- src/OUICheck.py
+++ src/OUICheck.py
@@ -1,23 +1,14 @@
 import json
 import argparse
+import requests
 
-    
-def get_manufacturer_name(manufacturer):
-    return manufacturer.get('companyName')
-
-def get_manufacturer_oui(manufacturer):
-    return manufacturer.get('oui')
-
+# MAC address given as an argument should be in the format 'XX:XX:XX:XX:XX:XX'
 parser = argparse.ArgumentParser()
 parser.add_argument("MAC_Address", type=str, help="MAC Address to verify the OUI for")
 args = parser.parse_args()
 
-manufacturerList = []
-with open('macaddress.io-db.json') as f:
-    for jsonObj in f:
-        manufacturerDict = json.loads(jsonObj)
-        manufacturerList.append(manufacturerDict)
+def get_info(mac_address):
+	return requests.get("https://macvendors.co/api/vendorname/"+mac_address+"/").text
 
-for i in manufacturerList:
-    if i['oui'] == args.MAC_Address.upper() :
-        print("MAC Manufacturer match : " + i['oui']+" "+i['companyName']+"\n")
+jsonResult = get_info(args.MAC_Address.upper())
+print(jsonResult)
--- src/arpTable.py
+++ src/arpTable.py
@@ -1,20 +1,17 @@
-'''
-Import OS to detect windows or Linux
-    command = "ip neigh" arp table on linux
-    command = "arp -a" arp table on Windows
+import subprocess
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("OS", type=str, help="OS you are currently using the script on (i.e. 'linux' for Linux and 'windows' for Windows")
+args = parser.parse_args()
 
-import requests
-
-def get_info(mac_address):
-	return requests.get("https://macvendors.co/api/vendorname/"+mac_address+"/").text
-'''
-
-# commandOutput = str(subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)).split('\\n')
-# routerIP = commandOutput[0].split()[0][2:]
-# routerMAC = commandOutput[0].split()[4]
-
-
-# commandOutput = out.split('\\r\\n')
-# routerIP = commandOutput[3].split()[0]
-# routerMAC = commandOutput[3].split(
+if args.OS == 'linux':
+    commandOutput = str(subprocess.check_output("ip neigh", shell=True, stderr=subprocess.STDOUT)).split('\\n')
+    routerIP = commandOutput[0].split()[0][2:]
+    routerMAC = commandOutput[0].split()[4]
+    print(routerIP)
+    print(routerMAC)
+elif args.OS == 'windows': # Either remove this or test it using windows
+    commandOutput = out.split('\\r\\n') # command = "arp -a" arp table on Windows ; where do I specify this ?
+    routerIP = commandOutput[3].split()[0]
+    routerMAC = commandOutput[3].split()
