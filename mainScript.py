906cae38ea4125632d031ec014f8f76986d882e1
KevinZiadeh <kevinziadeh@gmail.com>
2020-12-07 14:25:03 +0200

    Optimized for demo

--- mainScript.py
+++ mainScript.py
@@ -4,8 +4,12 @@ from src import SNaSSInitialize
 import settings
 
 # Execute Sequence Number and Signal Strength
+SNaSSInitialize.initialize('src/packets/SniffedPackets.txt')
+for sa in settings.clients:
+    SNaSSInitialize.plot(settings.clients[sa], sa)
+    break
+
 SNaSSInitialize.initialize('src/packets/SniffedPacketsSpoofed.txt')
-# SNaSSInitialize.initialize('src/packets/SniffedPackets.txt')
 for sa in settings.clients:
     if settings.clients[sa]["warning"] > -0.2 and (len(settings.clients[sa]["seqNum"])) > 100:
         print(sa + "is most definetely a spoofed mac address")
@@ -13,10 +17,12 @@ for sa in settings.clients:
         if decision == "y":
             SNaSSInitialize.plot(settings.clients[sa], sa)
     elif settings.clients[sa]["warning"] > -1 and (len(settings.clients[sa]["seqNum"])) > 100:
-        print(sa + "might be a spoofed mac address")
+        print(sa + " might be a spoofed mac address")
         decision = input("Plot figure? (y yes, n no)").lower()
         if decision == "y":
             SNaSSInitialize.plot(settings.clients[sa], sa)
+
+
 
 # Execute OUI Detection
 # OUIDetectionUsingTextFiles.executeSimulation()
